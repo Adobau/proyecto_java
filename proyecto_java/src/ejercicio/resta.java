2fabf34a60ced62479e28026c62b2948f203c1ba
Alejandro16 <adonisbautista39@gmail.com>
2020-05-16 19:11:14 -0700

    eh agregado el codigo a la clase resta

--- proyecto_java/src/ejercicio/resta.java
+++ proyecto_java/src/ejercicio/resta.java
@@ -5,5 +5,25 @@ import java.util.Scanner;
 public class resta {
     public static void main(String[] args) {
         
-    }
+    
+   Scanner leer = new Scanner(System.in);
+      
+    double numero1, numero2, R;
+    
+        System.out.println("-----RESTA-----");
+        
+        System.out.println("Digite el primer valor");
+        numero1 = leer.nextDouble();
+        
+         System.out.println("Digite el segundo valor");
+        numero2 = leer.nextDouble();
+        
+        R = numero1 - numero2;
+        
+        System.out.println("-----------------------");
+        System.out.println("Resultado" + R);
+        System.out.println("-----------------------");
+        
+        
+    }  
 }
